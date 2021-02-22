419bb4eb927990ad4ed814e39ac2e1d5269acc07
Cake <78360666+CakeA@users.noreply.github.com>
2021-02-22 19:51:49 +0100

    Added shop search events (max. 32 pages & 32 furnitures)

---
+++ src/Game/Shop/Events/OnShopSearch.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+
+using Newtonsoft.Json.Linq;
+
+using MySql.Data.MySqlClient;
+
+using Server.Game.Users;
+using Server.Game.Rooms;
+
+using Server.Socket.Clients;
+using Server.Socket.Events;
+using Server.Socket.Messages;
+
+using Server.Game.Furnitures;
+using Server.Game.Shop.Furnitures;
+
+namespace Server.Game.Shop.Events {
+    class OnShopSearch : ISocketEvent {
+        public string Event => "OnShopSearch";
+
+        public int Execute(SocketClient client, JToken data) {
+            string input = data.ToString().ToLower();
+
+            List<GameShopPage> pages = new List<GameShopPage>();
+
+            foreach(GameShopPage page in Shop.GameShop.Pages.FindAll(x => (x.Title != null && x.Title.ToLower().Contains(input)) || (x.Description != null && x.Description.ToLower().Contains(input)))) {
+                if(pages.Count > 32)
+                    break;
+
+                pages.Add(page);
+            }
+
+            List<GameShopFurniture> furnitures = new List<GameShopFurniture>();
+
+            foreach(GameShopFurniture furniture in Shop.GameShop.Furnitures.FindAll(x => (x.Furniture.Title != null && x.Furniture.Title.ToLower().Contains(input)) || (x.Furniture.Description != null && x.Furniture.Description.ToLower().Contains(input)))) {
+                if(furnitures.Count > 32)
+                    break;
+
+                furnitures.Add(furniture);
+            }
+
+            client.Send(new SocketMessage("OnShopSearch", new {
+                pages, furnitures
+            }).Compose());
+
+            return 1;
+        }
+    }
+}
