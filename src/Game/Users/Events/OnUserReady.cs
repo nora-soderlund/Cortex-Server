10ffc6c8af3fa68a0a644582499fd7c8c1278da3
Cake <78360666+CakeA@users.noreply.github.com>
2021-02-25 01:37:48 +0100

    Changed structure of user furnitures

--- src/Game/Users/Events/OnUserReady.cs
+++ src/Game/Users/Events/OnUserReady.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
 
 using Server.Game.Users;
+using Server.Game.Users.Furnitures;
 using Server.Game.Rooms;
 using Server.Game.Rooms.Users;
 
@@ -16,9 +17,34 @@ namespace Server.Game.Users.Events {
         public string Event => "OnUserReady";
 
         public int Execute(SocketClient client, JToken data) {
-            if(client.User.Room == null && client.User.Home != null) {
-                GameRoomManager.AddUser((int)client.User.Home, client.User);
+            Dictionary<string, Dictionary<string, int>> furnitures = new Dictionary<string, Dictionary<string, int>>();
+
+            foreach(GameUserFurniture furniture in client.User.Furnitures) {
+                if(!furnitures.ContainsKey(furniture.Furniture.Id))
+                    furnitures.Add(furniture.Furniture.Id, new Dictionary<string, int>());
+
+                if(furniture.Room == 0) {
+                    if(!furnitures[furniture.Furniture.Id].ContainsKey("room"))
+                        furnitures[furniture.Furniture.Id].Add("room", 0);
+
+                    furnitures[furniture.Furniture.Id]["room"]++;
+                }
+                else {
+                    if(!furnitures[furniture.Furniture.Id].ContainsKey("inventory"))
+                        furnitures[furniture.Furniture.Id].Add("inventory", 0);
+
+                    furnitures[furniture.Furniture.Id]["inventory"]++;
+                }
             }
+
+            client.Send(new SocketMessage("OnUserUpdate", new {
+                home = client.User.Home,
+
+                furnitures = furnitures
+            }).Compose());
+
+            if(client.User.Room == null && client.User.Home != null)
+                GameRoomManager.AddUser((int)client.User.Home, client.User);
 
             return 1;
         }
