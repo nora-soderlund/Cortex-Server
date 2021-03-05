4741574fb8612f8ef5daf24100b0e2f0cd5309f9
Cake <78360666+CakeA@users.noreply.github.com>
2021-03-05 06:34:42 +0100

    Added home room update when disconnecting

--- src/Game/Rooms/Users/Events/OnSocketClose.cs
+++ src/Game/Rooms/Users/Events/OnSocketClose.cs
@@ -2,6 +2,8 @@ using System;
 
 using Newtonsoft.Json.Linq;
 
+using MySql.Data.MySqlClient;
+
 using Server.Game.Users;
 using Server.Game.Rooms;
 
@@ -15,6 +17,17 @@ namespace Server.Game.Rooms.Users.Events {
         public int Execute(SocketClient client, JToken data) {
             if(client.User.Room == null)
                 return 0;
+
+            using(MySqlConnection connection = new MySqlConnection(Program.Connection)) {
+                connection.Open();
+
+                using(MySqlCommand command = new MySqlCommand("UPDATE users SET home = @home WHERE id = @id", connection)) {
+                    command.Parameters.AddWithValue("@home", client.User.Room.Id);
+                    command.Parameters.AddWithValue("@id", client.User.Id);
+
+                    command.ExecuteNonQuery();
+                }
+            }
 
             GameRoomUser roomUser = client.User.Room.Users.Find(x => x.Id == client.User.Id);
 
