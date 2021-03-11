40736a168863194c3b8a3fb6a965eda2e32f7a40
Cake <78360666+CakeA@users.noreply.github.com>
2021-03-11 16:03:54 +0100

    Added furniture video player controls

--- src/Game/Furnitures/GameFurnitureManager.cs
+++ src/Game/Furnitures/GameFurnitureManager.cs
@@ -80,26 +80,20 @@ namespace Server.Game.Furnitures {
             if(video != null)
                 return video;
 
-            try {
-                using MySqlConnection connection = new MySqlConnection(Program.Connection);
-                connection.Open();
+            using MySqlConnection connection = new MySqlConnection(Program.Connection);
+            connection.Open();
 
-                using MySqlCommand command = new MySqlCommand("SELECT * FROM furniture_videos WHERE id = @id", connection);
-                command.Parameters.AddWithValue("@id", id);
+            using MySqlCommand command = new MySqlCommand("SELECT * FROM furniture_videos WHERE id = @id", connection);
+            command.Parameters.AddWithValue("@id", id);
 
-                using MySqlDataReader reader = command.ExecuteReader();
+            using MySqlDataReader reader = command.ExecuteReader();
 
-                if(!reader.Read())
-                    return DownloadVideo(id);
+            if(!reader.Read())
+                return DownloadVideo(id);
 
-                video = new GameFurnitureVideo(reader);
+            video = new GameFurnitureVideo(reader);
 
-                Videos.Add(video);
-            }
-            catch(Exception exception) {
-                if(Program.Discord != null)
-                    Program.Discord.Exception(exception);
-            }
+            Videos.Add(video);
 
             return video;
         }
--- src/Game/Rooms/Furnitures/Logics/Effects/GameRoomFurnitureVideo.cs
+++ src/Game/Rooms/Furnitures/Logics/Effects/GameRoomFurnitureVideo.cs
@@ -28,7 +28,7 @@ namespace Server.Game.Rooms.Furnitures.Logics {
         public int Interval => 1000;
         public int IntervalCount { get; set; }
 
-        public bool Enabled = true;
+        public bool Enabled = false;
 
         public int Video = 0;
         public int VideoTime = 0;
@@ -40,9 +40,10 @@ namespace Server.Game.Rooms.Furnitures.Logics {
             if(Furniture.Extra == null || Furniture.Extra.Length == 0)
                 return;
 
+            Enabled = true;
+
             string[] videos = Furniture.Extra.Split(',');
 
-            
             ThreadPool.QueueUserWorkItem((a) => {
                 foreach(string video in videos)
                     Videos.Add(GameFurnitureManager.GetVideo(video));
@@ -90,6 +91,172 @@ namespace Server.Game.Rooms.Furnitures.Logics {
         }
 
         public void OnUserUse(GameRoomUser user, JToken data) {
+            if(!user.HasRights())
+                return;
+
+            if(data["action"] != null) {
+                string action = data["action"].ToString();
+
+                if(data["video"] == null) {
+                    if(Videos.Count == 0) {
+                        user.User.Client.Send(new SocketMessage("OnRoomFurnitureUse", false).Compose());
+
+                        return;
+                    }
+
+                    if(action == "next") {
+                        if(Videos.Count < 2) {
+                            user.User.Client.Send(new SocketMessage("OnRoomFurnitureUse", false).Compose());
+
+                            return;
+                        }
+
+                        Video++;
+
+                        VideoTime = 0;
+
+                        if(Video >= Videos.Count)
+                            Video = 0;
+
+                        Furniture.Room.Send(new SocketMessage("OnRoomFurnitureVideoStart", new {
+                            id = Furniture.Id,
+
+                            video = Videos[Video].Id,
+                            time = VideoTime
+                        }).Compose());
+
+                        user.User.Client.Send(new SocketMessage("OnRoomFurnitureUse", true).Compose());
+                    }
+                    else if(action == "previous") {
+                        if(Videos.Count < 2) {
+                            user.User.Client.Send(new SocketMessage("OnRoomFurnitureUse", false).Compose());
+
+                            return;
+                        }
+
+                        Video--;
+
+                        VideoTime = 0;
+
+                        if(Video == -1)
+                            Video = Videos.Count - 1;
+
+                        Furniture.Room.Send(new SocketMessage("OnRoomFurnitureVideoStart", new {
+                            id = Furniture.Id,
+
+                            video = Videos[Video].Id,
+                            time = VideoTime
+                        }).Compose());
+
+                        user.User.Client.Send(new SocketMessage("OnRoomFurnitureUse", true).Compose());
+                    }
+                    else if(action == "stop") {
+                        user.User.Client.Send(new SocketMessage("OnRoomFurnitureUse", false).Compose());
+
+                        Enabled = false;
+
+                        Video = 0;
+                        VideoTime = 0;
+
+                        Furniture.Room.Send(new SocketMessage("OnRoomFurnitureVideoStop", Furniture.Id).Compose());
+
+                        user.User.Client.Send(new SocketMessage("OnRoomFurnitureUse", true).Compose());
+                    }
+                    else if(action == "pause") {
+                        user.User.Client.Send(new SocketMessage("OnRoomFurnitureUse", false).Compose());
+
+                        if(!Enabled) {
+                            Furniture.Room.Send(new SocketMessage("OnRoomFurnitureVideoStart", new {
+                                id = Furniture.Id,
+
+                                video = Videos[Video].Id,
+                                time = VideoTime
+                            }).Compose());
+                        }
+                        else
+                            Furniture.Room.Send(new SocketMessage("OnRoomFurnitureVideoStop", Furniture.Id).Compose());
+
+                        Enabled = !Enabled;
+
+                        user.User.Client.Send(new SocketMessage("OnRoomFurnitureUse", true).Compose());
+                    }
+                }
+                else {
+                    string video = data["video"].ToString();
+
+                    if(action == "add") {
+                        try {
+                            GameFurnitureVideo furnitureVideo = GameFurnitureManager.GetVideo(video);
+
+                            if(Videos.Find(x => x.Id == furnitureVideo.Id) != null) {
+                                user.User.Client.Send(new SocketMessage("OnRoomFurnitureUse", false).Compose());
+
+                                return;
+                            }
+
+                            Videos.Add(furnitureVideo);
+
+                            Enabled = true;
+
+                            Video = Videos.IndexOf(furnitureVideo);
+                            VideoTime = 0;
+
+                            Furniture.Room.Send(new SocketMessage("OnRoomFurnitureVideoStart", new {
+                                id = Furniture.Id,
+
+                                video = Videos[Video].Id,
+                                time = VideoTime
+                            }).Compose());
+
+                            UpdateFurnitureExtra(); 
+
+                            user.User.Client.Send(new SocketMessage("OnRoomFurnitureUse", true).Compose());
+                        }
+                        catch(Exception exception) {
+                            if(Program.Discord != null)
+                                Program.Discord.Exception(exception);
+
+                            user.User.Client.Send(new SocketMessage("OnRoomFurnitureUse", false).Compose());
+                        }
+                    }
+                    else if(action == "remove") {
+                        GameFurnitureVideo furnitureVideo = Videos.Find(x => x.Id == video);
+
+                        if(furnitureVideo == null)
+                            return;
+
+                        int index = Videos.IndexOf(furnitureVideo);
+
+                        Videos.Remove(furnitureVideo);
+
+                        if(Video == index) {
+                            VideoTime = 0;
+
+                            if(Videos.Count != 0) {
+                                Video++;
+
+                                if(Video >= Videos.Count)
+                                    Video = 0;
+
+                                user.User.Client.Send(new SocketMessage("OnRoomFurnitureVideoStart", new {
+                                    id = Furniture.Id,
+
+                                    video = Videos[Video].Id
+                                }).Compose());
+                            }
+                            else
+                                Furniture.Room.Send(new SocketMessage("OnRoomFurnitureVideoStop", Furniture.Id).Compose());
+                        }
+
+                        UpdateFurnitureExtra();
+
+                        user.User.Client.Send(new SocketMessage("OnRoomFurnitureUse", true).Compose());
+                    }
+                }
+
+                return;
+            }
+
             user.User.Client.Send(new SocketMessage("OnRoomFurnitureVideoUse", new {
                 id = Furniture.Id,
 
@@ -99,18 +266,26 @@ namespace Server.Game.Rooms.Furnitures.Logics {
 
                 enabled = Enabled
             }).Compose());
+        }
 
-            /*if(!Enabled) {
-                Furniture.Room.Send(new SocketMessage("OnRoomFurnitureVideoStart", new {
-                    id = Furniture.Id,
+        public void UpdateFurnitureExtra() {
+            Furniture.Extra = "";
 
-                    link = "http://api.project-cortex.net/YouTube/" + Furniture.Extra + ".mp4"
-                }).Compose());
+            foreach(GameFurnitureVideo video in Videos) {
+                if(Furniture.Extra.Length != 0)
+                    Furniture.Extra += ",";
+
+                Furniture.Extra += video.Id;
             }
-            else
-                Furniture.Room.Send(new SocketMessage("OnRoomFurnitureVideoStop", Furniture.Id).Compose());
-        
-            Enabled = !Enabled;*/
+
+            using MySqlConnection connection = new MySqlConnection(Program.Connection);
+            connection.Open();
+
+            using MySqlCommand command = new MySqlCommand("UPDATE room_furnitures SET extra = @extra WHERE id = @id", connection);
+            command.Parameters.AddWithValue("@extra", Furniture.Extra);
+            command.Parameters.AddWithValue("@id", Furniture.Id);
+
+            command.ExecuteNonQuery();
         }
     }
 }
