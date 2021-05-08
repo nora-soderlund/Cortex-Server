c02c9fde0fcad87c34bf03f8067b87c33e68d357
Cake <78360666+CakeA@users.noreply.github.com>
2021-05-08 09:23:14 +0200

    Added room material update events

--- src/Game/Rooms/Settings/GameRoomSettings.cs
+++ src/Game/Rooms/Settings/GameRoomSettings.cs
@@ -59,6 +59,52 @@ namespace Server.Game.Rooms.Settings {
                     client.User.Room.SetDescription(description);
                 }
 
+                if(data["floor"] != null) {
+                    if(data["floor"]["material"] != null)
+                        client.User.Room.FloorMaterial = data["floor"]["material"].ToString();
+
+                    if(data["floor"]["thickness"] != null)
+                        client.User.Room.FloorThickness = data["floor"]["thickness"].ToObject<int>();
+
+                    client.User.Room.Send(new SocketMessage("OnRoomSettingsUpdate", new { floor_material = client.User.Room.FloorMaterial, floor_thickness = client.User.Room.FloorThickness }).Compose());
+
+                    using(MySqlConnection connection = new MySqlConnection(Program.Connection)) {
+                        connection.Open();
+
+                        using(MySqlCommand command = new MySqlCommand("UPDATE rooms SET floor_material = @floor_material, floor_thickness = @floor_thickness WHERE id = @id", connection)) {
+                            command.Parameters.AddWithValue("@id", client.User.Room.Id);
+
+                            command.Parameters.AddWithValue("@floor_material", client.User.Room.FloorMaterial);
+                            command.Parameters.AddWithValue("@floor_thickness", client.User.Room.FloorThickness);
+
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                }
+
+                if(data["wall"] != null) {
+                    if(data["wall"]["material"] != null)
+                        client.User.Room.WallMaterial = data["wall"]["material"].ToString();
+
+                    if(data["wall"]["thickness"] != null)
+                        client.User.Room.WallThickness = data["wall"]["thickness"].ToObject<int>();
+
+                    client.User.Room.Send(new SocketMessage("OnRoomSettingsUpdate", new { wall_material = client.User.Room.WallMaterial, wall_thickness = client.User.Room.WallThickness }).Compose());
+
+                    using(MySqlConnection connection = new MySqlConnection(Program.Connection)) {
+                        connection.Open();
+
+                        using(MySqlCommand command = new MySqlCommand("UPDATE rooms SET wall_material = @wall_material, wall_thickness = @wall_thickness WHERE id = @id", connection)) {
+                            command.Parameters.AddWithValue("@id", client.User.Room.Id);
+                            
+                            command.Parameters.AddWithValue("@wall_material", client.User.Room.WallMaterial);
+                            command.Parameters.AddWithValue("@wall_thickness", client.User.Room.WallThickness);
+
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                }
+
                 return 1;
             }
         }
